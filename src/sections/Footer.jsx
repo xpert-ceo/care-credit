6e0ae9ad7ba17ba06b05674cc5db5b570d3b8127
xpert-ceo <franklinejiogu4@gmail.com>
2026-06-29 18:31:49 +0100

    Update Loans link to point to CareCredit application

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -78,7 +78,7 @@ const Footer = () => {
             <div className="mt-6 flex flex-col gap-4 text-white/60">
 
               <a
-                href="#loans"
+                href="httsp://account.carecreditmfb.com/apply-loan"
                 className="hover:text-white transition"
               >
                 Loans
