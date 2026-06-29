4fa4e2890757d1d3bd7a80296f6b1969f26fef37
xpert-ceo <franklinejiogu4@gmail.com>
2026-06-29 22:18:01 +0100

    Fix typo in loan application URL

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -78,7 +78,7 @@ const Footer = () => {
             <div className="mt-6 flex flex-col gap-4 text-white/60">
 
               <a
-                href="httsp://account.carecreditmfb.com/apply-loan"
+                href="https://account.carecreditmfb.com/apply-loan"
                 className="hover:text-white transition"
               >
                 Loans
